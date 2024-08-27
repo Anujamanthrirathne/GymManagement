a19c652cf43c936a575c01cb34f91a0a1f46bde8
anuja <anujamanthriratne@gmail.com>
2024-08-27 11:13:00 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+// src/App.js
 import './App.css';
+import Footer from './Components/Footer/Footer';
+import Hero from './Components/Hero/Hero';
+import Programs from './Components/Hero/Programs/Programs';
+import Reasons from './Components/Hero/Reasons/Reasons';
+import Join from './Components/Join/Join';
+import Plans from './Components/Plans/plans';
+import Testimonials from './Components/Testimonials/Testimonials';
+
 function App() {
   return (
     <div className="App">
-          <span style={{color: 'white'}}>Subscribe Zainkeepscode</span>
+      <Hero/>
+      <Programs/>
+      <Reasons/>
+      <Plans/> 
+      <Testimonials/>
+      <Join/>
+      <Footer/>
     </div>
   );
 }
